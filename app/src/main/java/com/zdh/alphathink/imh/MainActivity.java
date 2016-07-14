94208265be957186d3e9b2c896b914c645d16713
郑鼎浩 <zdh@alphathink.org>
2016-07-14 13:19:23 +0800

    7.14

---
+++ app/src/main/java/com/zdh/alphathink/imh/MainActivity.java
@@ -0,0 +1,22 @@
+package com.zdh.alphathink.imh;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View v) {
+        Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/zdh/alphathink/imh/WelcomeActivity.java
@@ -0,0 +1,58 @@
+package com.zdh.alphathink.imh;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+
+    private  int[] mImgIds = new int[]{R.drawable.guide_image1,
+                                          R.drawable.guide_image2,
+                                          R.drawable.guide_image3};
+    private List<ImageView> mImages = new ArrayList<ImageView>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        getSupportActionBar().hide();
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                ImageView imageView = new ImageView(WelcomeActivity.this);
+                imageView.setImageResource(mImgIds[position]);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                container.addView(imageView);
+                mImages.add(imageView);
+                return imageView;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(mImages.get(position));
+            }
+
+            @Override
+            public int getCount() {
+                return mImgIds.length;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+        });
+    }
+    //创建适配器
+
+}
